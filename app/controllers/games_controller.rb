ca152d0b1b3ee9674f9877d3e56549ac0c8fd6b7
sebastien sorrentino <sebastien.sorrentino23@gmail.com>
2022-12-16 14:51:49 +0100

    RAILSLongestword

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,28 @@
+require "open-uri"
+
 class GamesController < ApplicationController
   def new
     alphabet = ("A".."Z").to_a
     @letters = alphabet.sample(10)
   end
 
+
 def score
-  @word = params[:word]
-  @letters = params[:letters]
-  url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+  @letters = params[:letters].split
+  @word = (params[:word] || "").upcase
+  @included = included?(@word, @letters)
+  @english_word = english_word?(@word)
+end
+
+private
+
+def included?(word, letters)
+  word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+end
+
+def english_word?(word)
+  response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+  json = JSON.parse(response.read)
+  json['found']
 end
 end
